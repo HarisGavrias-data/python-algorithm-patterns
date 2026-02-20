9429fe39c131fceac89079f9ec126cf7c8ac8f33
Charalampos Gavrias <70964644+HarisGavrias-data@users.noreply.github.com>
2026-02-20 13:26:50 +0200

    Update number_of_islands.py

--- algos/dfs-bfs/number_of_islands.py
+++ algos/dfs-bfs/number_of_islands.py
@@ -39,19 +39,20 @@ def number_of_islands(grid):
     visited = set()
     islands = 0
 
-    def dfs(r, c):
+    def neighboirs(r, c):
         if (r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0" or (r, c) in visited):
             return
         visited.add((r, c))
-        dfs(r+1, c)
-        dfs(r-1, c)
-        dfs(r, c+1)
-        dfs(r, c-1)
+        neighboirs(r+1, c)
+        neighboirs(r-1, c)
+        neighboirs(r, c+1)
+        neighboirs(r, c-1)
 
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == "1" and (r, c) not in visited:
                 islands += 1
-                dfs(r, c)
+                neighboirs(r, c)
 
     return islands
+ islands
